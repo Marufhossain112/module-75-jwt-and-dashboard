51c17cc93bb859bb2fcee62abcc3302d2ddc53e8
maruf <dev.maruf112@gmail.com>
2022-11-20 13:03:21 +0600

    create and verify with jwt token

--- src/Pages/MyAppointments/MyAppointments.js
+++ src/Pages/MyAppointments/MyAppointments.js
@@ -1,16 +1,18 @@
-import { async } from "@firebase/util";
 import { useQuery } from "@tanstack/react-query";
 import React, { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthProvider";
 
 const MyAppointments = () => {
   const { user } = useContext(AuthContext);
-  //   const url = `http://localhost:5000/bookings?email=${user?.email}`;
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
   const { data: bookings = [] } = useQuery({
     queryKey: ["bookings", user?.email],
     queryFn: async () => {
-      const res = await fetch(url);
+      const res = await fetch(url, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("token")}`,
+        },
+      });
       const data = await res.json();
       return data;
     },
