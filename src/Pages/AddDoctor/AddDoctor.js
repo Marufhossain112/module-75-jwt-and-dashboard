0cc5538f0a10862d65a49a7a84e1069d37ed40f9
maruf <dev.maruf112@gmail.com>
2022-11-21 21:27:52 +0600

    add doctor form

--- src/Pages/AddDoctor/AddDoctor.js
+++ src/Pages/AddDoctor/AddDoctor.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 const AddDoctor = () => {
-    return (
-        <div>
-            <h2>Add a Doctor</h2>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleAddDoctor = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <div className="w-96 p-7">
+        <h2 className="text-xl text-center">Sign Up</h2>
+        <form onSubmit={handleSubmit(handleAddDoctor)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              {" "}
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              {...register("name", {
+                required: "Name is Required",
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              {" "}
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", {
+                required: true,
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              {" "}
+              <span className="label-text">Specialty</span>
+            </label>
+            <select className="select select-bordered w-full max-w-xs">
+              <option disabled selected>
+                Pick a specialty
+              </option>
+              <option>Han Solo</option>
+              <option>Greedo</option>
+            </select>
+          </div>
+          <input
+            className="btn btn-accent w-full mt-4"
+            value="Sign Up"
+            type="submit"
+          />
+          {/* {signUpError && <p className="text-red-600">{signUpError}</p>} */}
+        </form>
+        <p>
+          Already have an account{" "}
+          <Link className="text-secondary" to="/login">
+            Please Login
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+      </div>
+    </div>
+  );
 };
 
 export default AddDoctor;
