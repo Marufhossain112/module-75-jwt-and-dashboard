91d1a781aaa52581f7f6b2a821ef20cd51e07048
maruf <dev.maruf112@gmail.com>
2022-11-22 00:39:29 +0600

    load manage  doctor data to ui

--- src/Pages/ManageDoctor/ManageDoctor.js
+++ src/Pages/ManageDoctor/ManageDoctor.js
@@ -1,6 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const ManageDoctor = () => {
+  const { data: manageDoctorData = [] } = useQuery({
+    queryKey: ["doctor"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/doctor");
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(manageDoctorData);
   return (
     <div>
       <h2 className="text-3xl">Manage Doctor</h2>
