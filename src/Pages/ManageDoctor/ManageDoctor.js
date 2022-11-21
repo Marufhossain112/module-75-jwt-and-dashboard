701a09b05c6bf55fea23aceac18bda21aae14b1f
maruf <dev.maruf112@gmail.com>
2022-11-22 01:59:34 +0600

    show users on table

--- src/Pages/ManageDoctor/ManageDoctor.js
+++ src/Pages/ManageDoctor/ManageDoctor.js
@@ -14,6 +14,36 @@ const ManageDoctor = () => {
   return (
     <div>
       <h2 className="text-3xl">Manage Doctor</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Avatar</th>
+              <th>Email</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {manageDoctorData.map((doctor, i) => (
+              <tr key={doctor._id}>
+                <th>{i + 1}</th>
+                <td>{doctor.name}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="w-24 rounded-full">
+                      <img src={doctor.image} alt="any" />
+                    </div>
+                  </div>
+                </td>
+                <td>{doctor.email}</td>
+                <td>{<button className="btn btn-sm">Delete</button>}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
