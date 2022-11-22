db918042929ea7d068eb5dd772bfbfc1c4bb18e8
maruf <dev.maruf112@gmail.com>
2022-11-22 16:51:03 +0600

    stripe set up

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,10 @@
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+const stripePromise = loadStripe(
+  "pk_test_51M6sUXGGuleKu5Js1MoQrS6AXlPuL2XREt4GEycGi2pYEgznQwwEhEJlMbfgvnhKP7ue8TiECsUj3UAFxrTkXNge00mlODxGxO"
+);
+console.log(stripePromise);
 
 const Payment = () => {
   const data = useLoaderData();
