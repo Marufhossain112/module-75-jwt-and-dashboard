3befe04005ec1017948c8437227c132fee8c0f4c
maruf <dev.maruf112@gmail.com>
2022-11-22 14:13:30 +0600

    show payment data

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -7,6 +7,14 @@ const Payment = () => {
   return (
     <div>
       <h2 className="text-3xl">Payment</h2>
+      <div>
+        <h2>You have appointment on {data.treatment}</h2>
+        <h3>
+          Your price is <strong>${data.price}</strong>
+        </h3>
+        <p>Date - {data.appointmentDate}</p>
+        <p>Slot- {data.slot}</p>
+      </div>
     </div>
   );
 };
